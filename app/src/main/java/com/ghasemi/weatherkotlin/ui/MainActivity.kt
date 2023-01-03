a94d4d4084333932cdc5938d0d7b727600394436
Aliakbar <aliakbar.ghasemi1371@gmail.com>
2023-01-03 11:24:18 +0330

    add simple bottom navigation

---
+++ app/src/main/java/com/ghasemi/weatherkotlin/ui/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.ghasemi.weatherkotlin.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.ghasemi.weatherkotlin.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.currentWeatherFragment, R.id.futureListWeatherFragment, R.id.settingsFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        nav_view.setupWithNavController(navController)
+    }
+}
--- app/src/main/java/com/ghasemi/weatherkotlin/ui/main/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.ghasemi.weatherkotlin.ui.main
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
